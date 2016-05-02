8dfa9b89047d421c3ffb131b2af472428d89a45f
losethequit <losethequit@gmail.com>
2016-05-02 17:31:29 -0400

    hotfix - 29

--- app.js
+++ app.js
@@ -364,6 +364,10 @@ app.post('/instaInputQuery', function (req, res, next) {
         }
     });
 
+    //
+
+
+
 });
 
 ////https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10
