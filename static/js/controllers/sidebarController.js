9438b45e26abc591b3b1bcccae4bf92f2450a7d7
Brendan Carr <Brendan Carr>
2016-05-17 08:58:59 -0400

    hotfix - 43

--- static/js/controllers/sidebarController.js
+++ static/js/controllers/sidebarController.js
@@ -5,4 +5,6 @@ console.log("OUTSIDE sidebarController");
 angular.module("mainModule")
     .controller('sidebarController', function ($scope, instagramService, twitterService, alloy) {
         console.log("INSIDE sidebarController");
+
+        $scope.twitterMenuOptions = ['Mark', 'Tom', 'Travis'], ['Mark', 'Tom', 'Travis'], ['Mark', 'Tom', 'Travis'];
     });
